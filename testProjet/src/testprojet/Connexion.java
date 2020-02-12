e06560efa9713ebe8cf2093b05155e3e8c702555
Ryff99 <58586050+Ryff99@users.noreply.github.com>
2020-02-12 17:03:51 +0100

    test du projet connexion à une bdd

---
+++ testProjet/src/testprojet/Connexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojet;
+import java.sql.*;
+
+/**
+ *
+ * @author Robin
+ */
+public class Connexion {
+    public static Connection connecterDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver ok");
+            String url="jdbc:mysql://localhost:3306/bibliotheque";
+            String userName ="root";
+            String password="root";
+            Connection con=DriverManager.getConnection(url,userName,password);
+            System.out.println("Connexion effectuée");
+            return con;
+        }catch(Exception e){ 
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ testProjet/src/testprojet/TestProjet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojet;
+import java.sql.*;
+
+import javax.swing.*;
+import testprojet.Connexion.*;
+import static testprojet.Connexion.connecterDB;
+/**
+ *
+ * @author Robin
+ */
+public class TestProjet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        Connection cnx = connecterDB();
+        Statement state = cnx.createStatement();
+      //L'objet ResultSet contient le résultat de la requête SQL
+      ResultSet result = state.executeQuery("SELECT * FROM livre;");
+      //On récupère les MetaData
+      ResultSetMetaData resultMeta = result.getMetaData();
+         
+      System.out.println("\n**********************************");
+      //On affiche le nom des colonnes
+      for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+        System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+         
+      System.out.println("\n**********************************");
+         
+      while(result.next()){         
+        for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+          System.out.print("\t" + result.getObject(i).toString() + "\t |");
+           
+        System.out.println("\n---------------------------------");
+        
+      }
+      String s = result.getObject(2).toString();
+      System.out.println(s);
+      
+    }
+    
+          
+    
+}
